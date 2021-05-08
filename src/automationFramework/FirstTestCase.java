6cbbfc1fdea3501e982ed6036d9d8e7596abced2
Sofia Nayak <nayaksofia1989@gmail.com>
2021-05-08 18:18:28 +0530

    add selenium .jar files

---
+++ src/automationFramework/FirstTestCase.java
@@ -0,0 +1,25 @@
+package automationFramework;
+
+public class FirstTestCase {
+	//FirstTest: Is For Google Home Page Test.
+	
+	public static void main(String[] args) {
+		 
+		//1. Formulate A Base Test URL
+
+		//2. Locate A Web Driver BY Locating the path
+
+		//3. Set Selenium System Properties.
+
+		//4. Instsntiate Selenium Web Driver . That means Create an object of the web driver.
+
+		//5. Launch Browser
+
+		//6. Perform Test Evaluation 
+
+		//7. Close Driver 
+
+
+	}
+
+}
